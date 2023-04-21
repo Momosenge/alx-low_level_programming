88c211a06949550858e4844186b9249cf6039e34
Momosenge <mpumisenge@gmail.com>
2023-04-21 08:16:20 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char momo;
+
+	for (momo = "a"; momo <= "2"; momo++)
+		putchar(momo);
+
+	putchar('\n');
+	return (0);
+}
