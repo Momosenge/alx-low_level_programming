734c0f375e9f9b1d3f59a11a1579a51c85e42065
Momosenge <mpumisenge@gmail.com>
2023-04-27 05:02:30 -0700

    test

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,15 @@
+#ifndef "main.h"
+#define "main.h"
+#include <stdio.h>
+/**
+ * file: main.h
+ * author: Momosenge
+ * Header file containig all prototypes of functions used in 0x03- Debugging
+ */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
